f090e383d5dbb4fa5ccd19a2de82b9b1b0c71e6c
Maresias <ale.maresias@gmail.com>
2024-10-18 23:32:26 -0300

    users routes

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,26 @@
+import { knex } from '../database'
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { randomUUID } from 'crypto'
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.post('/', async (request, reply) => {
+    const createUserSchemaBody = z.object({
+      name: z.string(),
+      email: z.string(),
+      password: z.string(),
+    })
+
+    const { name, email, password } = createUserSchemaBody.parse(request.body)
+
+    await knex('users').insert({
+      id: randomUUID(),
+      session_id: randomUUID(),
+      name,
+      email,
+      password,
+    })
+
+    return reply.status(200).send()
+  })
+}
