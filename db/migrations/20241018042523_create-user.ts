b8c22db5b57a318a5826ed89a5195b2a586744b4
Maresias <ale.maresias@gmail.com>
2024-10-21 20:54:57 -0300

    update db

--- db/migrations/20241018042523_create-user.ts
+++ db/migrations/20241018042523_create-user.ts
@@ -3,7 +3,6 @@ import type { Knex } from 'knex'
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('users', (table) => {
     table.uuid('id').primary()
-    table.uuid('session_id').notNullable().index()
     table.text('name').notNullable()
     table.text('email').notNullable()
     table.text('password').notNullable()
--- db/migrations/20241018044150_create-daily-diet.ts
+++ db/migrations/20241018044150_create-daily-diet.ts
@@ -2,14 +2,14 @@ import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('daily_diet', (table) => {
-    table.uuid('id').primary()
+    table.uuid('id').notNullable()
     table.text('name').notNullable()
+    table.uuid('session_id').notNullable()
     table.text('description').notNullable()
-    table.boolean('my_diet_is_ok').notNullable()
-    table.text('user_id').references('id').inTable('users')
+    table.decimal('my_diet_is_ok').notNullable()
 
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
-    table.timestamp('updated_at').defaultTo(knex.fn.now())
+    table.timestamp('updated_at')
   })
 }
 
