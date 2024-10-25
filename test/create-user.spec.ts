27107adf7f4b446b5ce408ee29574086e7412289
Maresias <ale.maresias@gmail.com>
2024-10-25 01:48:11 -0300

    test must be able to create a user

--- test/create-user.spec.ts
+++ test/create-user.spec.ts
@@ -1,10 +1,29 @@
-import { it, describe } from 'vitest'
+import { it, describe, beforeAll, afterAll, beforeEach } from 'vitest'
+import { execSync } from 'child_process'
 import request from 'supertest'
 import { app } from '../src/app'
 
 describe('Create user', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  beforeEach(async () => {
+    execSync('npm run knex -- migrate:rollback --all')
+    execSync('npm run knex -- migrate:latest')
+  })
   it('must be able to create a user', async () => {
     await request(app.server)
-    .
+      .post('/user')
+      .send({
+        name: 'zazau',
+        email: 'zazau@gmail.com',
+        password: '123',
+      })
+      .expect(200)
   })
 })
